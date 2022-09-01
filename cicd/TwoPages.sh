f1437c16de55c0df971db8348bd9821ac48db93b
Rob Brown <robert.will.brown@gmail.com>
2022-09-01 12:42:13 +0100

    Fix script

--- cicd/TwoPages.sh
+++ cicd/TwoPages.sh
@@ -6,6 +6,8 @@ set -e
 
 NumPages=$(pdfinfo video-streaming-cheatsheet.pdf |grep "^Pages:" |awk '{print $2}')
 
+echo "Number of pages = ${NumPages}"
+
 if [ ${NumPages} -ne 2 ]; then
 	echo "Failed check for number of pages = 2.  Number of pages found: ${NumPages}"
 	exit 1
